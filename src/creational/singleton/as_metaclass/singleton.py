0d5b0041482931c52ae449c0ece29dda6cbdffc9
Jesus Carretero Rodríguez <jesus.carretero@feverup.com>
2025-08-10 20:51:52 +0200

    fix(SingletonMeta): supress non-relevant mypy errors

--- src/creational/singleton/as_metaclass/singleton.py
+++ src/creational/singleton/as_metaclass/singleton.py
@@ -10,9 +10,9 @@ class SingletonMeta(type):
     It supports inheritance but is not thread-safe.
     """
 
-    __instances: dict[type[T], T] = {}
+    __instances: dict[type[T], T] = {}  # type: ignore[valid-type]
 
     def __call__(cls: type[T], *args, **kwargs) -> T:
         if cls not in cls.__instances:  # type: ignore[attr-defined]
-            cls.__instances[cls] = super().__call__(*args, **kwargs)  # type: ignore[attr-defined]
+            cls.__instances[cls] = super().__call__(*args, **kwargs)  # type: ignore[misc, attr-defined]
         return cls.__instances[cls]  # type: ignore[attr-defined]

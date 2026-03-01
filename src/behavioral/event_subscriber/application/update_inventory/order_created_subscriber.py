e6a76b2c6fb7f58f34ef493c9909421da8e829c8
Jesus Carretero Rodríguez <jesus.carretero@feverup.com>
2026-03-01 20:04:36 +0100

    chore(OrderCreatedSubscriber): delete empty slots

--- src/behavioral/event_subscriber/application/update_inventory/order_created_subscriber.py
+++ src/behavioral/event_subscriber/application/update_inventory/order_created_subscriber.py
@@ -3,8 +3,6 @@ from behavioral.event_subscriber.domain.subscriber import Subscriber
 
 
 class OrderCreatedSubscriber(Subscriber[OrderCreatedEvent]):
-    __slots__ = ()
-
     def consume(self, event: OrderCreatedEvent) -> None:
         print(f"Updating inventory for created order: {event.order_id}")
 
